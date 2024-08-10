fb1643d6b29c534080b46ee0be6004185f6905c7
Tanmay Kejriwal <tanmaykejriwal28@gmail.com>
2024-08-10 16:38:58 -0500

    Added vercel Analytics

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Analytics } from "@vercel/analytics/react";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
